c021626503420403542460ca584a0d127a481f6e
XiaXia <82567254+XiaXia78@users.noreply.github.com>
2025-02-17 17:10:23 +0800

    Update AQA.py

--- PoseSequenceCompare/AQA.py
+++ PoseSequenceCompare/AQA.py
@@ -5,7 +5,10 @@ from PoseSequenceCompare.tools import DTW as caculate_distance
 instructive_pose="instructive_pose/ChensTaiChi.npz"
 if __name__ == '__main__':
     pose1=np.load(instructive_pose)['reconstruction']
-    video=input()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video-path", type=str, default="video.mp4", help="Path to your practice video.")
+    opts = parser.parse_args()
+    video=opts.video_path
     pose2=video2pose(videopath=video)
     score=caculate_distance(pose1,pose2,method='itakura',options={'max_slope':2.0})
     print(score)
